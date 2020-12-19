817ad857973b893947296b53c5f6de0cf53405c5
Duncan Brown <iamduncanbrown@gmail.com>
2020-12-19 20:48:05 +0000

    Initial plugin files added.

---
+++ includes/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Misc functions
+ *
+ * @package WPGraphQL\Dokan
+ * @since 0.0.1
+ */
+
+namespace WPGraphQL\Dokan\Functions;
+
+function setup_minor_integrations() {
+    // TODO: add minor integration
+}
